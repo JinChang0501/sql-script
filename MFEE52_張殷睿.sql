89132a30046bb72f2f97570a6de3e665e68b8898
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-04-19 20:03:17 +0800

    作業

--- MFEE52_張殷睿.sql
+++ MFEE52_張殷睿.sql
@@ -13,6 +13,17 @@ select id, pname from full_passengers where id >= 591 and id <= 883;
 select * from full_passengers
 where pname like 'Andersson, %';
 
+-- 計算票數量
+select ticket, max(ticket) from full_passengers
+group by ticket;
+
+
+
+select name,survived, max((select count(ticket) from full_passengers where pname like 'Andersson, %')) from full_passengers
+group by ticket;
+
+select ticket, count(ticket) from full_passengers
+group by ticket;
 
 -- 4.	承上題，已知Afrida還有一個已婚的妹妹叫做Anna，請找出Anna與其丈夫小孩一家三口的全部資料
 -- <<提示一>>已結婚的女性姓名欄中仍會註記娘家姓氏
@@ -23,13 +34,41 @@ where pname like 'Andersson, %';
 -- 5.	找出所有名字是Leonard的男性乘客，顯示id, pclass, pname
 -- <<提示>>共7位，不是8位
 select id, pclass, pname from full_passengers
-where pname like '%Leonard%' and sex = 'male';
+where pname like '%Leonard%' and sex = 'male' and not pname like 'Leonard%';
+
+
+
+-- 6.	查詢所有乘客持有的票券中，最多人持有的那一種ticket，回傳票券名稱(ticket)與持有人數(ticket_count)兩個欄位
+-- <<提示>>共11人持有
+
+select ticket, count(ticket) as ticket_count from full_passengers
+group by ticket
+order by ticket_count desc limit 1;
+
+-- 7.	找出二等客艙以及三等客艙中所有男性乘客的平均年齡
+-- <<提示>>小心邏輯運算子的使用
+
+select pclass, avg(age) as '平均年齡' from full_passengers
+where sex = 'male' and pclass in (2,3)
+group by pclass;
+
+
+-- 8.	列出所有登船點的登船人數與百分比，僅列出有明確登船地點的資料即可
+-- <<提示一>>3列3欄，3欄位名稱分別為登船點、登船人數、登船點佔百分比
+-- <<提示二>>select round(3.14159, 2);
+select embarked as 登船點, count(embarked) as 登船人數, round(count(embarked) / (select count(id) from full_passengers where embarked != '')*100, 2) as ratio from full_passengers
+where embarked != ''
+group by embarked;
+
+
+select round(3.14159, 3) from full_passengers;
+
+
+
 
 show warnings;
 
-select * from full_passengers;
-
-
+select count(id) from full_passengers;
 
 
 
